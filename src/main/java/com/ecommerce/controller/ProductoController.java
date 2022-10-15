41f4a5af1037caf22658a853cdf380dfc12d4ea7
Franco Demetrio <francodemetrio87@gmail.com>
2022-10-15 16:17:28 -0300

    Funcionalidad para editar productos

--- src/main/java/com/ecommerce/controller/ProductoController.java
+++ src/main/java/com/ecommerce/controller/ProductoController.java
@@ -8,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/productos")
@@ -37,6 +40,25 @@ public class ProductoController {
         Usuario u = new Usuario(1, "", "", "", "", "", "", "");
         producto.setUsuario(u);
         service.save(producto);
+        return "redirect:/productos";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model) {
+        new Producto();
+        Producto caco;
+        Optional<Producto> optProducto = service.get(id);
+        caco = optProducto.get();
+
+        LOGGER.info("Producto buscado: {}", caco);
+        model.addAttribute("producto", caco);
+
+        return "productos/edit";
+    }
+
+    @PostMapping("/update")
+    public String update(Producto producto) {
+        service.update(producto);
         return "redirect:/productos";
     }
 }
--- src/main/java/com/ecommerce/model/Producto.java
+++ src/main/java/com/ecommerce/model/Producto.java
@@ -95,8 +95,6 @@ public class Producto {
                 ", descripcion='" + descripcion + '\'' +
                 ", imagen='" + imagen + '\'' +
                 ", precio='" + precio + '\'' +
-                ", cantidad='" + cantidad + '\'' +
-                ", usuario=" + usuario +
-                '}';
+                ", cantidad='" + cantidad + '\'' ;
     }
 }
